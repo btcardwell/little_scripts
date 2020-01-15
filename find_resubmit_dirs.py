3c51b516ac741453d678276ac978f31735c8b98d
Bryan Cardwell <bryan.cardwell@cern.ch>
2020-01-15 08:34:03 -0600

    fix comment

--- find_resubmit_dirs.py
+++ find_resubmit_dirs.py
@@ -16,11 +16,10 @@ else:
     print "Please specify a condor directory with -w"
     sys.exit(1)
 
-# find all directories with an associated root file and no resubmit file
+
 dir_contents = [condor_dir + d for d in os.listdir(condor_dir)]
 subdirs = filter(os.path.isdir, dir_contents)
 dirs_with_resubmit = [d for d in subdirs if os.path.isfile(d + '/condor_resubmit.sub')]
 
 for d in dirs_with_resubmit:
     print os.path.split(d)[1]
-
